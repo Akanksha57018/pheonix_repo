a3d98746cc098af4a59d22cbc9d3f02403897da8
Akanksha57018 <apandit04@gmail.com>
2019-07-20 11:43:23 -0700

    Update the Demo file

--- Demo.java
+++ Demo.java
@@ -4,4 +4,11 @@ private int bookId;
 private String title;
 private String author;
 private float price;
+public void setbookId(int bookId)
+{
+this.bookId=bookId;
 }
+public int getbookId()
+{
+return bookId;
+}}
